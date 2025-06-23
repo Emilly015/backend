b116df6453702c9cf6d3c6271952c0510935ea49
emilly015 <emilly.v.duarte01@gmail.com>
2025-06-23 11:18:49 -0300

    ok, faltando algumas coisas

--- alunos/admin.py
+++ alunos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Estado, Cidade
+from .models import Estado, Cidade, Alunos
 
 admin.site.register(Estado)
 admin.site.register(Cidade)
+admin.site.register(Alunos)
--- alunos/models.py
+++ alunos/models.py
@@ -14,3 +14,11 @@ class Cidade(models.Model):
     def __str__(self):
         return self.nome
 
+class Alunos(models.Model):
+    nome = models.CharField(max_length=80)
+    email = models.EmailField(max_length=50)
+    dataNascimento = models.DateField()
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.nome} - {self.cidade}'
